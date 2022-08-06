b0f3f39910a8bcda4d605887361acb64b65e73f3
syahruncega@gmail.com <syahruncega@gmail.com>
2022-08-06 13:15:02 +0800

    Model bidan, kartu keluarga

--- lib/models/bidan.dart
+++ lib/models/bidan.dart
@@ -16,41 +16,43 @@ import 'package:moms_care_mobile/models/user.dart';
 part 'bidan.freezed.dart';
 part 'bidan.g.dart';
 
-List<Bidan> listBidanFromJson(String str) => List<Bidan>.from(json.decode(str).map((x) => Bidan.fromJson(x)));
+List<Bidan> listBidanFromJson(String str) =>
+    List<Bidan>.from(json.decode(str).map((x) => Bidan.fromJson(x)));
 
-String listBidanToJson(List<Bidan> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+String listBidanToJson(List<Bidan> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 @freezed
 class Bidan with _$Bidan {
-    const factory Bidan({
-        required String id,
-        required String userId,
-        required String nik,
-        required String namaLengkap,
-        required String jenisKelamin,
-        required String tempatLahir,
-        required DateTime tanggalLahir,
-        required String agamaId,
-        required String tujuhAngkaTerakhirStr,
-        required String nomorHp,
-        required String email,
-        required String alamat,
-        required String desaKelurahanId,
-        required String kecamatanId,
-        required String kabupatenKotaId,
-        required String provinsiId,
-        String? fotoProfil,
-        DateTime? createdAt,
-        DateTime? updatedAt,
-        DateTime? deletedAt,
-        User? user,
-        Provinsi? provinsi,
-        KabupatenKota? kabupatenKota,
-        Kecamatan? kecamatan,
-        DesaKelurahan? desaKelurahan,
-        List<LokasiTugas>? lokasiTugas,
-        Agama? agama,
-    }) = _Bidan;
+  const factory Bidan({
+    required String id,
+    required String userId,
+    required String nik,
+    required String namaLengkap,
+    required String jenisKelamin,
+    required String tempatLahir,
+    required DateTime tanggalLahir,
+    required String agamaId,
+    required String tujuhAngkaTerakhirStr,
+    required String nomorHp,
+    required String email,
+    required String alamat,
+    required String desaKelurahanId,
+    required String kecamatanId,
+    required String kabupatenKotaId,
+    required String provinsiId,
+    String? fotoProfil,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+    DateTime? deletedAt,
+    User? user,
+    Provinsi? provinsi,
+    KabupatenKota? kabupatenKota,
+    Kecamatan? kecamatan,
+    DesaKelurahan? desaKelurahan,
+    List<LokasiTugas>? lokasiTugas,
+    Agama? agama,
+  }) = _Bidan;
 
-    factory Bidan.fromJson(Map<String, dynamic> json) => _$BidanFromJson(json);
+  factory Bidan.fromJson(Map<String, dynamic> json) => _$BidanFromJson(json);
 }
--- lib/models/kartu_keluarga.dart
+++ lib/models/kartu_keluarga.dart
@@ -10,38 +10,42 @@ import 'package:moms_care_mobile/models/provinsi.dart';
 part 'kartu_keluarga.freezed.dart';
 part 'kartu_keluarga.g.dart';
 
-List<KartuKeluarga> listKartuKeluargaFromJson(String str) => List<KartuKeluarga>.from(json.decode(str).map((x) => KartuKeluarga.fromJson(x)));
+List<KartuKeluarga> listKartuKeluargaFromJson(String str) =>
+    List<KartuKeluarga>.from(
+        json.decode(str).map((x) => KartuKeluarga.fromJson(x)));
 
-String listKartuKeluargaToJson(List<KartuKeluarga> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+String listKartuKeluargaToJson(List<KartuKeluarga> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 @freezed
- class KartuKeluarga with _$KartuKeluarga {
-    const factory KartuKeluarga({
-        required String id,
-        required String bidanId,
-        required String nomorKk,
-        required String namaKepalaKeluarga,
-        required String alamat,
-        required String rt,
-        required String rw,
-        required String kodePos,
-        required String desaKelurahanId,
-        required String kecamatanId,
-        required String kabupatenKotaId,
-        required String provinsiId,
-         String? fileKk,
-        required String isValid,
-        required DateTime tanggalValidasi,
-         String? alasanDitolak,
-        DateTime? createdAt,
-        DateTime? updatedAt,
-        DateTime? deletedAt,
-        Provinsi? provinsi,
-        KabupatenKota? kabupatenKota,
-        Kecamatan? kecamatan,
-        DesaKelurahan? desaKelurahan,
-        Bidan? bidan,
-    }) = _KartuKeluarga;
+class KartuKeluarga with _$KartuKeluarga {
+  const factory KartuKeluarga({
+    required String id,
+    required String bidanId,
+    required String nomorKk,
+    required String namaKepalaKeluarga,
+    required String alamat,
+    required String rt,
+    required String rw,
+    required String kodePos,
+    required String desaKelurahanId,
+    required String kecamatanId,
+    required String kabupatenKotaId,
+    required String provinsiId,
+    String? fileKk,
+    required String isValid,
+    required DateTime tanggalValidasi,
+    String? alasanDitolak,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+    DateTime? deletedAt,
+    Provinsi? provinsi,
+    KabupatenKota? kabupatenKota,
+    Kecamatan? kecamatan,
+    DesaKelurahan? desaKelurahan,
+    Bidan? bidan,
+  }) = _KartuKeluarga;
 
-    factory KartuKeluarga.fromJson(Map< String, dynamic> json) => _$KartuKeluargaFromJson(json);
+  factory KartuKeluarga.fromJson(Map<String, dynamic> json) =>
+      _$KartuKeluargaFromJson(json);
 }
